bcd555c0e760e05e747e93facef530981e2eb1cf
dodo <dmukunzi0@gmail.com>
2017-11-18 21:57:42 +0200

    Make the login form

--- src/components/LoginFrom.js
+++ src/components/LoginFrom.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
-import { Card } from './common';
+import { Card, CardSection, Input, Button } from './common';
 
 class LoginForm extends Component {
     render() {
         return (
-            <Card></Card>
+            <Card>
+                <CardSection>
+                    <Input
+                        label="Email"
+                        placeholder="email@mail.com"
+                    />
+                </CardSection>
+                <CardSection>
+                    <Input
+                        secureTextEntry
+                        label="Password"
+                        placeholder="password"
+                    />
+                </CardSection>
+                <CardSection>
+                    <Button>
+                        Login
+                    </Button>
+                </CardSection>
+            </Card>
         );
     }
 }
