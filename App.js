21677d85aab58c567f11a4e62f5d99ef8533fb23
dodo <dmukunzi0@gmail.com>
2017-11-18 21:19:51 +0200

    Set Up Firebase

--- App.js
+++ App.js
@@ -8,14 +8,29 @@ import {
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './src/reducers';
+import firebase from 'firebase';
 
 export default class App extends Component {
+  componentWillMount() {
+    // Initialize Firebase
+    const config = {
+      apiKey: "AIzaSyDqJumHTmjgs3YKsqc13wSuvRaty2TPRc4",
+      authDomain: "manager-9ef8c.firebaseapp.com",
+      databaseURL: "https://manager-9ef8c.firebaseio.com",
+      projectId: "manager-9ef8c",
+      storageBucket: "manager-9ef8c.appspot.com",
+      messagingSenderId: "199278808963"
+    };
+    firebase.initializeApp(config);
+  }
+
+  // Render App
   render() {
     return (
       <Provider store={createStore(reducers)}>
         <View style={{ flex: 1 }}>
           <Text>
-            Hellooo
+            Hellooo Dodooo!
         </Text>
         </View>
       </Provider>
