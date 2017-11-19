6a87a54db57347d44784eae7c9fc8754fc612269
dodo <dmukunzi0@gmail.com>
2017-11-19 09:53:56 +0200

    Add from inputs on Employee create

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,14 +1,43 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { Card, CardSection, Input, Button } from './common';
+import { connect } from 'react-redux';
+import { employeeUpdate } from '../actions';
 
 class EmployeeCreate extends Component {
     render() {
+        const { name, phone, shift } = this.props;
         return (
-            <View>
-                <Text>New Employee</Text>
-            </View>
+            <Card>
+                <CardSection>
+                    <Input
+                        label="Name"
+                        placeholder="John Doe"
+                        value={name}
+                        onChangeText={value => this.props.employeeUpdate({ prop: 'name', value })}
+                    />
+                </CardSection>
+                <CardSection>
+                    <Input
+                        label="Phone"
+                        placeholder="+250 784 587 487"
+                        value={phone}
+                        onChangeText={value => this.props.employeeUpdate({ prop: 'phone', value })}
+                    />
+                </CardSection>
+                <CardSection />
+                <CardSection>
+                    <Button>
+                        Hire!
+                    </Button>
+                </CardSection>
+            </Card>
         );
     }
 }
 
-export default EmployeeCreate;
+const mapStateToProps = (state) => {
+    const { name, phone, shift } = state.employeeForm;
+    return { name, phone, shift };
+};
+
+export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate);  
